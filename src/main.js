f08e875cf498f17e5f17d09a463ff5fb7ae5df7d
carlosjorge7 <carlosjorgech7@gmail.com>
2021-10-19 12:47:53 +0200

    vue app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// importamos bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css'
+
 createApp(App).mount('#app')
